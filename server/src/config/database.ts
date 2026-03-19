c9a5d2cc6e7859fbe368b7e5a80dcf5e098bc2e5
GraindeRiz <gregoryly45@gmail.com>
2026-03-19 15:08:32 +0100

    Migration Sequelize

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -1,5 +1,6 @@
 import { Sequelize } from 'sequelize';
-
+import 'dotenv/config';
+console.log(process.env.DATABASE_URL)
 const sequelize = process.env.DATABASE_URL
   ? new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres",
@@ -11,20 +12,22 @@ const sequelize = process.env.DATABASE_URL
     },
     logging: false,
   })
-  : new Sequelize({
-    username: process.env.DB_USER || "postgres",
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME || "postgres",
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT || 5432),
-    dialect: "postgres",
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
+  : new Sequelize(
+    process.env.DB_NAME || "postgres",
+    process.env.DB_USER || "postgres",
+    process.env.DB_PASSWORD || "",
+    {
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT || 5432),
+      dialect: "postgres",
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
       },
-    },
-  });
-logging: false,
+      logging: false,
+    }
+  );
 
 export default sequelize;
