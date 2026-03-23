ff2475e64539b0db8cf04a1b2783be902e6d5435
GraindeRiz <gregoryly45@gmail.com>
2026-03-23 09:17:05 +0100

    retest cd

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,37 @@
 import './App.css'
-
 import { useEffect, useState } from "react";
-// Définition d'une interface pour le typage
-// Sera couvert plus en profondeur en TH
+
 interface User {
   id: number;
-  name: string;
+  firstName: string;
+  lastName: string;
 }
+
 function App() {
-  // 1. Définition de l'état
   const [data, setData] = useState<User[]>([]);
-  // 2. Appel API au montage du composant
+
   useEffect(() => {
-    fetch(`${import.meta.env.VITE_API_URL}/users`)
+    fetch(`${import.meta.env.VITE_API_URL}/api/users`)
       .then(res => res.json())
-      .then(result => setData(result))
+      .then(result => {
+        console.log(result);
+        setData(result);
+      })
       .catch(err => console.error(err));
   }, []);
-  // 3. Rendu (JSX)
+
   return (
     <div>
       <ul>
         <h1>Liste des utilisateurs</h1>
         {data.map((item) => (
-          <li key={item.id}>{item.name}</li>
+          <li key={item.id}>{item.firstName} {item.lastName}</li>
         ))}
         <h2>Total: {data.length}</h2>
-        <p>LE TEST ULTIME POUR CD ACTION AAAHHHHH</p>
+        <p>LE CD FONCTIONNE OOUUUUUU</p>
       </ul>
     </div>
   );
 }
+
 export default App;
