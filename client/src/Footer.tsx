998b3b22e4922ad34ba92720ecdc894fc4d4221e
Erreur_504 <stefantroch@gmail.com>
2026-04-18 09:03:15 +0200

    Stage intermédiaire - les débuts de tailwind #29

---
+++ client/src/Footer.tsx
@@ -0,0 +1,10 @@
+function Footer(){
+
+    return(
+            <footer>
+                <p className="text-xl font-semibold text-red-600">{new Date().getFullYear()}  &copy; Groupe GGS</p>
+            </footer>
+    );
+}
+
+export default Footer
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    // Injecter les variables d'environnement au build
+    __API_URL__: JSON.stringify(process.env.VITE_API_URL || 'http://localhost:3000/api'),
+  },
 })
